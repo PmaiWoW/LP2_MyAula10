32c01a26fe7d393ba406511a933035304db7e0ce
PmaiWoW <PmaiWoW@Gmail.com>
2019-12-11 17:53:40 +0000

    Modify Program class of Indexers exercise slightly and add incomplete exercise 5 (OperOver)

--- Indexers/Program.cs
+++ Indexers/Program.cs
@@ -9,10 +9,10 @@ namespace Indexers
             MyVector vec1 = new MyVector(1.5f, 3.7f);
             MyVector vec2 = new MyVector(7.6f, 9.1f);
 
-            Console.WriteLine($"vector 1 X: {vec1[0]}/{vec1.X}");
-            Console.WriteLine($"vector 1 X: {vec1[1]}/{vec1.X}");
-            Console.WriteLine($"vector 2 X: {vec2[0]}/{vec2.X}");
-            Console.WriteLine($"vector 2 X: {vec2[1]}/{vec2.X}");
+            Console.WriteLine($"vector 1 X: {vec1[0]}/{vec1.X}" + 
+                $"\nvector 1 Y: {vec1[1]}/{vec1.Y}" + 
+                $"\nvector 2 X: {vec2[0]}/{vec2.X}" + 
+                $"\nvector 2 Y: {vec2[1]}/{vec2.Y}");
 
             vec1[0] = 666.666f;
 
@@ -22,6 +22,11 @@ namespace Indexers
             vec1["b"] = 50.5f;
             vec2["a"] = 60.5f;
             vec2["1"] = 50.5f;
+
+            Console.WriteLine($"vector 1 X: {vec1["x"]}/{vec1["a"]}" +
+                $"vector 1 Y: {vec1["y"]}/{vec1["b"]}" +
+                $"vector 2 X: {vec2["a"]}/{vec2["0"]}" +
+                $"vector 2 Y: {vec2["b"]}/{vec2["1"]}");
         }
     }
 }
---
+++ OperOver/Vector3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OperOver
+{
+    public struct Vector3
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+        public float Magnitude =>
+            MathF.Sqrt(MathF.Pow(X, X) + MathF.Pow(X, X) + MathF.Pow(Z, Z));
+
+        public Vector3(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public static bool operator >(Vector3 v1, Vector3 v2) =>
+            v1.Magnitude > v2.Magnitude;
+
+        public static bool operator <(Vector3 v1, Vector3 v2) =>
+            v1.Magnitude < v2.Magnitude;
+    }
+}
